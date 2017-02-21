3b178b1850da687e1069edf73668722175084018
gosher <macciak@gmail.com>
2017-02-21 20:53:32 +0000

    created article controller, new and create method

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  root 'pages#index'
 
   get 'pages/contact'
 
   get 'pages/about'
+  
+  resources:articles
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
