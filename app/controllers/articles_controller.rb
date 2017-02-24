cad979a962155207ccc581e54a2ce3b6614c699e
gosher <macciak@gmail.com>
2017-02-24 07:56:36 +0000

    added errors and validation partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
         #render plain: params[:article].inspect
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = 'Thanks ! Your article has been saved.'
+            flash[:success] = 'Thanks ! Your article has been saved.'
             redirect_to article_path(@article)
         else
             render 'new'
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
     
     def update
         if @article.update(article_params)
-            flash[:notice] = 'Yeah !!! Article successfully updated'
+            flash[:success] = 'Yeah !!! Article successfully updated'
             redirect_to article_path(@article)
         else
             render 'edit'
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
     
     def destroy
         @article.destroy
-        flash[:notice] = 'Article was successfully deleted'
+        flash[:danger] = 'Article was successfully deleted'
         redirect_to articles_path
     end
     
