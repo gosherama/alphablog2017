2828db0d24df3cb89d0403897e31ea017cf50fc6
gosher <macciak@gmail.com>
2017-03-01 08:00:59 +0000

    add association btw User and Article and add debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
+    belongs_to :user
     #Ensures that there is a title before saving the article in DB
     validates :title, presence: true, length: {minimum: 3, maximum: 50 }
     validates :description, presence: true, length: {minimum: 10, maximum: 300 }
+    validates :user_id, presence: true
 end
